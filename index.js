566464ac4adba400a0729a7222b089442637911e
Javi <nb2javi@gmail.com>
2023-08-19 17:20:59 -0500

    Successfully generating SVGs

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { Triangle, Circle, Square } = require("./lib/shapes");
 
-// const generateSVG = (({}))
+const generateSVG = (shape, text) =>
+  `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg>"
+    ${shape}
+
+    ${text}
+
+    </svg>
+  `;
 
 inquirer
   .prompt([
@@ -14,7 +21,7 @@ inquirer
     },
     {
       type: "input",
-      name: "color",
+      name: "shapeColor",
       message:
         "What color will the shape be? Enter either regular color name or hexidecimal.",
     },
@@ -32,4 +39,41 @@ inquirer
   ])
   .then((answers) => {
     console.log(answers);
+    let chosenShape;
+    switch (answers.shape) {
+      case "circle":
+        console.log("circle!");
+        chosenShape = new Circle(
+          answers.text,
+          answers.textColor,
+          answers.shapeColor
+        );
+        break;
+      case "square":
+        console.log("square!");
+        chosenShape = new Square(
+          answers.text,
+          answers.textColor,
+          answers.shapeColor
+        );
+        break;
+      case "triangle":
+        console.log("triangle!");
+        chosenShape = new Triangle(
+          answers.text,
+          answers.textColor,
+          answers.shapeColor
+        );
+        break;
+    }
+    // console.log(generateSVG(chosenShape.render(), chosenShape.textRender()));
+    console.log(chosenShape.render());
+    console.log(chosenShape.textRender());
+    const shape = chosenShape.render();
+    const text = chosenShape.textRender();
+    const completedSvg = generateSVG(shape, text);
+    // console.log(completedSvg);
+    fs.writeFile("examplesvg.svg", completedSvg, (err) => {
+      err ? console.log(err) : console.log("successfully created svg!");
+    });
   });
