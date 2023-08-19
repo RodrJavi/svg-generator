65740ce89d819c195a3e4e6ea9bb8534f9a03af2
Javi <nb2javi@gmail.com>
2023-08-19 16:45:38 -0500

    Added inquirer questions and imported shape classes into index.js

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { Triangle, Circle, Square } = require("./lib/shapes");
 
 // const generateSVG = (({}))
 
 inquirer
   .prompt([
     {
+      type: "list",
+      name: "shape",
+      message: "What shape will the svg be?",
+      choices: ["circle", "square", "triangle"],
+    },
+    {
       type: "input",
-      name: "test",
-      message: "Is this working right?",
+      name: "color",
+      message:
+        "What color will the shape be? Enter either regular color name or hexidecimal.",
+    },
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to 3 characters for the text",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message:
+        "What color for the text? Enter either regular color name or hexidecimal.",
     },
   ])
   .then((answers) => {
